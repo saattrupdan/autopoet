1eed3fd5b66436607f12656b1bb0c64ae7cffc5c
Dan Nielsen <saattrupdan@gmail.com>
2019-09-30 08:50:17 +0100

    added comments and tidied up a little bit

--- trump.py
+++ trump.py
@@ -33,9 +33,9 @@ class SyllableCounter():
             A list of syllable counts.
         '''
         from tqdm import tqdm
-        pbar = tqdm(iter(docs), total = len(docs))
-        pbar.set_description('Counting syllables')
-        return list(map(self.count_doc, pbar))
+        with tqdm(iter(docs), total = len(docs)) as pbar:
+            pbar.set_description('Counting syllables')
+            return list(map(self.count_doc, pbar))
 
 class TrumpTweets():
     ''' Trump tweets from http://www.trumptwitterarchive.com/archive. '''
@@ -45,7 +45,7 @@ class TrumpTweets():
 
     def compile(self, fname_in = 'tweets.json', fname_out = 'tweets.csv'):
         ''' Load in the tweets from a json file, split into phrases, count
-            syllables and save as a csv file to be loaded by the load() method.
+            syllables and save as a csv file to be loaded by load().
     
             INPUT
                 fname_in = 'tweets.json'
@@ -61,22 +61,30 @@ class TrumpTweets():
         with open(fname_in, 'r') as file_in:
             tweets = json.load(file_in)
 
+        # Split into sentences, remove commas in numbers, trailing dots
+        # in the beginning of the sentences, and collapse multiple spaces
+        # into a single one
         tweets = (re.split(r' *[.,!]($| +)', 
                   re.sub(r'([0-9]),([0-9])', r'\1\2', 
                   re.sub(r'^\.+', '', 
                   re.sub(r' +', ' ', tweet['text']))))
                   for tweet in tweets)
+
+        # Concatenate all the sentences efficiently
         tweets = list(chain.from_iterable(tweets))
 
-        tweets = tweets
-
+        # Count syllables in the sentences and store in a dataframe
         counter = SyllableCounter()
         tweets = {'tweet': tweets, 'count': counter.count_docs(tweets)}
         tweets = pd.DataFrame(tweets)
+
+        # Remove both blank sentences and sentences that led the syllable
+        # count to encounter an error
         tweets = tweets[tweets['count'] > 0]
         tweets.dropna(inplace = True)
-        tweets.to_csv(fname_out, index = False)
 
+        # Save the sentences and store them in a class variable
+        tweets.to_csv(fname_out, index = False)
         self.tweets = tweets
 
         return self
@@ -104,6 +112,7 @@ class TrumpTweets():
         '''
         import random
         import re
+
         if syllables:
             phrases = self.tweets[self.tweets['count'] == syllables]['tweet']
         else:
@@ -112,9 +121,13 @@ class TrumpTweets():
         try:
             phrases = list(phrases)
             phrase = random.choice(list(phrases))
+
+            # Make the first letter in the phrase uppercase
             phrase = re.sub(r'^([a-z])', lambda m: m.group(1).upper(), phrase)
+
         except IndexError:
             phrase = f'<No phrase found with {syllables} syllables>'
+
         return phrase
 
     def haiku(self):
